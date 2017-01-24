f96b6780717051d247f8e0fbb19deb6052819134
ViniMartinPS <vinispfc@hotmail.com>
2017-01-24 10:05:28 -0200

    Fourth Commit

--- src/main/java/Exceptions/MensagemErro.java
+++ src/main/java/Exceptions/MensagemErro.java
@@ -4,9 +4,9 @@ public class MensagemErro {
 
 	public String getMessage(int errorCode){
 		switch(errorCode){
-		case 1: return "Erro de conexão com o Banco, por favor avise um administrador";
-		case 2: return "Erro de conexão com o Banco, por favor avise um administrador";
-		case 3: return "Erro de conexão com o Banco, por favor avise um administrador";
+		case 1: return "Erro de conexao com o Banco, por favor avise um administrador";
+		case 2: return "Erro de conexao com o Banco, por favor avise um administrador";
+		case 3: return "Erro de conexao com o Banco, por favor avise um administrador";
 		default : return "ERRO, avise um administrador";
 		}
 	}
