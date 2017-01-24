4868c0a40749b7398e27c3b79ce95898da38b144
ViniMartinPS <vinispfc@hotmail.com>
2017-01-24 10:03:40 -0200

    Third Commit

--- src/main/java/Dao/ConexaoBD.java
+++ src/main/java/Dao/ConexaoBD.java
@@ -14,7 +14,7 @@ public class ConexaoBD {
 				}
 			}
 			
-			/*public Connection obtemConexao() throws SQLException{
+			public Connection obtemConexao() throws SQLException{
 	System.setProperty("javax.net.ssl.keyStore","rds-combined-ca-bundle.pem");
 	System.setProperty("javax.net.ssl.trustStore","truststore.jks");
 	System.setProperty("javax.net.ssl.trustStorePassword", "juriteam");
@@ -22,14 +22,14 @@ public class ConexaoBD {
 			"jdbc:mysql://sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/h50iycg4e53jt7xi", 	//banco de dados
 			"xyi1h245niulusvv", 									//usuario ViniciusMartin << trabalho
 			"ixiy4hyhch15fsrj");									//senha
-}*/
+}
 
-	public Connection obtemConexao() throws SQLException{
+	/*public Connection obtemConexao() throws SQLException{
 				return DriverManager.getConnection(
 						"jdbc:mysql://localhost:3306/mercado_acoes_teste", 	//banco de dados
 						"root", 									//usuario ViniciusMartin << trabalho
 						"vinimartin");									//senha
-			}		
+			}		*/
 			
 }
 
